02b1e4ba81e45925933356b9e472b0b5ca2e6a19
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-26 20:01:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function wombo_combo(){
-    for(var i = 0; i < 500; i++){
+    for(var i = 0; i < 5000; i++){
         var btn = document.createElement('button');
         btn.innerHTML = 'button';
         var n = document.getElementsByTagName("body")[0];
