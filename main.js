9bed480905ad6edcceebedd7759d945677cce0bf
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-26 22:36:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-function wombo_combo(){
-    for(var i = 0; i < 5000; i++){
+function sleep(ms){
+    return new Promise(res => 
+        setTimeout(res, ms));
+}
+async function wombo_combo(){
+    var i = 0;
+    var context = new AudioContext();
+    var ms = 1000;
+    while(true){
         var btn = document.createElement('button');
-        btn.innerHTML = 'button';
+        i += 1;
+        btn.innerHTML = i;
+        btn.addEventListener('click', ()=>
+        {
+            var aud = document.getElementById("_audio");
+            aud.play()
+            ms = 10;
+        })
         var n = document.getElementsByTagName("body")[0];
+        await sleep(ms);
         n.appendChild(btn);
     };
 };
