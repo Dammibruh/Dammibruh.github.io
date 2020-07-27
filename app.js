06edc7d603ba87799c869dfa9ea4610c26008c70
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-27 03:35:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,29 +9,8 @@ var main = new Vue({
     uri: window.location.href
   },
   methods: {
-      random: function(ent, k){
-          var b = [];
-          for(var i = 0; i < k;i++){
-            var gen = Math.floor(Math.random()*ent.length);
-            b.push(ent[gen]);
-          }
-          return b;
-      },
-      request: function (method, url, data){
-          $.ajax({
-              method: method,
-              url: url,
-              data: data,
-              success: function(d){
-                  return d;
-              }
-          })
-      },
       shorten: function(){
           if(this.check()){
-            const digits = ['C', 'O', 'r', 'Y', 'V', 'i', 'j', 'z', 'h', 'y', '9', 'a', 'k', 'c', 'q', 'G', 'N', 'W', '6', 'b', 'Z', 'B', 'x', 'Q', '3', 'p', 'E', 'J', 'g', 'H', 'S', 'M', '5', 'f', 'n', 'm', 's', 'L', '0', 'l', '1', 'e', 'P', 'U', 'K', 'w', 'D', '8', 'I', 'v', 'T', '2', 'F', 'd', 'o', 't', 'A', '7', 'R', 'X', 'u', '4'];
-            var d = null;
-            const _shortened = this.random(digits, 6).join("");
             this.btn = "copy";
             $.ajax({
                 method: "post", 
@@ -75,7 +54,6 @@ var error_404 = new Vue({
             };
             const rgx =  "/https:\/\/mish.cf\/(?<thingy>.*)/i";
             var key = rgx.exec(window.location.href);
-            alert(key);
             $.ajax({
                 method: "get",
                 url: `https://mishortener.herokuapp.com/shortener/${key[1]}`,
