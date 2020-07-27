edec20998faee0a2876b39918ba4e3e3a64214f3
dammi <36568916+Dammibruh@users.noreply.github.com>
2020-07-27 02:52:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -74,6 +74,8 @@ var error_404 = new Vue({
                 this.sec = i;
             };
             var key = window.location.href.split("/")[3];
+            alert(key);
+            alert(window.location.href.split("/"));
             $.ajax({
                 method: "get",
                 url: `https://mishortener.herokuapp.com/shortener/${key}`,
